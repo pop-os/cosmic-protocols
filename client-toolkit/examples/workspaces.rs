262815d44a43d0fc7d525cfa74043dd6c987ec8e
Ian Douglas Scott <idscott@system76.com>
2022-12-14 13:42:00 -0800

    client-toolkit: Improve workspaces example

--- client-toolkit/examples/workspaces.rs
+++ client-toolkit/examples/workspaces.rs
@@ -1,8 +1,12 @@
 use cosmic_client_toolkit::workspace::{WorkspaceHandler, WorkspaceState};
-use sctk::registry::{ProvidesRegistryState, RegistryState};
-use wayland_client::{globals::registry_queue_init, Connection};
+use sctk::{
+    output::{OutputHandler, OutputState},
+    registry::{ProvidesRegistryState, RegistryState},
+};
+use wayland_client::{globals::registry_queue_init, protocol::wl_output, Connection, QueueHandle};
 
 struct AppData {
+    output_state: OutputState,
     registry_state: RegistryState,
     workspace_state: WorkspaceState,
 }
@@ -12,7 +16,38 @@ impl ProvidesRegistryState for AppData {
         &mut self.registry_state
     }
 
-    sctk::registry_handlers!();
+    sctk::registry_handlers!(OutputState);
+}
+
+// Need to bind output globals just so workspace can get output events
+impl OutputHandler for AppData {
+    fn output_state(&mut self) -> &mut OutputState {
+        &mut self.output_state
+    }
+
+    fn new_output(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        output: wl_output::WlOutput,
+    ) {
+    }
+
+    fn update_output(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _output: wl_output::WlOutput,
+    ) {
+    }
+
+    fn output_destroyed(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        output: wl_output::WlOutput,
+    ) {
+    }
 }
 
 impl WorkspaceHandler for AppData {
@@ -22,12 +57,23 @@ impl WorkspaceHandler for AppData {
 
     fn done(&mut self) {
         for group in self.workspace_state.workspace_groups() {
+            let output_name = group.output.as_ref().and_then(|output| {
+                let info = self.output_state.info(output).unwrap();
+                info.name.clone()
+            });
             println!(
-                "Group: capabilities: {:?}, output: {:?}",
-                &group.capabilities, &group.output
+                "Group: output: {}, capabilities: {:?}",
+                output_name.as_deref().unwrap_or("None"),
+                group.capabilities,
             );
             for workspace in &group.workspaces {
-                println!("{:?}", &workspace);
+                println!(
+                    "  Workspace: name: {}, coordinates: {:?}, capabilties: {:?}, state: {:?}",
+                    &workspace.name,
+                    &workspace.coordinates,
+                    &workspace.capabilities,
+                    &workspace.state
+                );
             }
         }
     }
@@ -40,6 +86,7 @@ fn main() {
 
     let registry_state = RegistryState::new(&globals);
     let mut app_data = AppData {
+        output_state: OutputState::new(&globals, &qh),
         workspace_state: WorkspaceState::new(&registry_state, &qh),
         registry_state,
     };
@@ -49,5 +96,6 @@ fn main() {
     }
 }
 
+sctk::delegate_output!(AppData);
 sctk::delegate_registry!(AppData);
 cosmic_client_toolkit::delegate_workspace!(AppData);

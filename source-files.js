ab90c60075ec62a1445a95da0df25f8a1fb7db66
ids1024 <ids1024@users.noreply.github.com>
2023-10-02 18:39:42 +0000

    deploy: 5c03417a08ef04755ad7bc2285d07968d89d69f8

--- source-files.js
+++ source-files.js
@@ -1,10 +1,10 @@
 var sourcesIndex = JSON.parse('{\
-"bitflags":["",[],["lib.rs"]],\
+"bitflags":["",[],["external.rs","internal.rs","iter.rs","lib.rs","parser.rs","public.rs","traits.rs"]],\
 "cfg_if":["",[],["lib.rs"]],\
 "cosmic_protocols":["",[],["lib.rs","protocol_macro.rs"]],\
 "dlib":["",[],["lib.rs"]],\
 "downcast_rs":["",[],["lib.rs"]],\
-"io_lifetimes":["",[],["example_ffi.rs","lib.rs","portability.rs","raw.rs","traits.rs","views.rs"]],\
+"io_lifetimes":["",[],["lib.rs","portability.rs","raw.rs","traits.rs","views.rs"]],\
 "libc":["",[["unix",[["linux_like",[["linux",[["arch",[["generic",[],["mod.rs"]]],["mod.rs"]],["gnu",[["b64",[["x86_64",[],["align.rs","mod.rs","not_x32.rs"]]],["mod.rs"]]],["align.rs","mod.rs"]]],["align.rs","mod.rs","non_exhaustive.rs"]]],["mod.rs"]]],["align.rs","mod.rs"]]],["fixed_width_ints.rs","lib.rs","macros.rs"]],\
 "libloading":["",[["os",[["unix",[],["consts.rs","mod.rs"]]],["mod.rs"]]],["changelog.rs","error.rs","lib.rs","safe.rs","util.rs"]],\
 "log":["",[],["__private_api.rs","lib.rs","macros.rs"]],\

547bf133c412b5391db84900a97698e9793315b1
Drakulix <Drakulix@users.noreply.github.com>
2024-01-03 19:16:20 +0000

    deploy: da8602e315b87066fdca811379b209568425d598

--- implementors/std/os/fd/raw/trait.AsRawFd.js
+++
@@ -1,4 +0,0 @@
-(function() {var implementors = {
-"nix":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.0/std/os/fd/raw/trait.AsRawFd.html\" title=\"trait std::os::fd::raw::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"nix/poll/struct.PollFd.html\" title=\"struct nix::poll::PollFd\">PollFd</a>"]],
-"wayland_server":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.0/std/os/fd/raw/trait.AsRawFd.html\" title=\"trait std::os::fd::raw::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"wayland_server/struct.ListeningSocket.html\" title=\"struct wayland_server::ListeningSocket\">ListeningSocket</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ type.impl/std/os/fd/raw/type.RawFd.js
@@ -0,0 +1,3 @@
+(function() {var type_impls = {
+"io_lifetimes":[]
+};if (window.register_type_impls) {window.register_type_impls(type_impls);} else {window.pending_type_impls = type_impls;}})()

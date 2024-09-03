d753341fb71b26a35c9d36911fed9c36bbff09f2
Drakulix <Drakulix@users.noreply.github.com>
2024-09-03 15:27:05 +0000

    deploy: ddc966a65c3a2de8c09f60616c1ec91794e2129b

--- wayland_protocols/wp/sidebar-items.js
+++ wayland_protocols/wp/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["linux_dmabuf","presentation_time","tablet","viewporter"]};
+window.SIDEBAR_ITEMS = {"mod":["alpha_modifier","content_type","drm_lease","fractional_scale","linux_dmabuf","linux_drm_syncobj","presentation_time","security_context","single_pixel_buffer","tablet","tearing_control","viewporter"]};

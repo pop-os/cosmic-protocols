ba22262b2c0db3ab91ae55bb8b190ced127bda68
Ian Douglas Scott <idscott@system76.com>
2025-01-15 16:43:40 -0800

    client-toolkit: Remove `gl` feature
    This was a helper for using the dmabuf export protocol, which is no
    longer provided.

--- client-toolkit/src/lib.rs
+++ client-toolkit/src/lib.rs
@@ -3,10 +3,6 @@ pub use sctk;
 pub use wayland_client;
 pub use wayland_protocols;
 
-#[cfg(feature = "gl")]
-pub mod egl;
-#[cfg(feature = "gl")]
-pub mod gl;
 pub mod screencopy;
 pub mod toplevel_info;
 pub mod toplevel_management;

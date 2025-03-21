8b18ae983a8d4e21bb27661c4aba298092275c48
Drakulix <Drakulix@users.noreply.github.com>
2025-03-21 16:44:14 +0000

    deploy: ee0d46f4b7e1508011a98225f14c4a0528ab2914

--- trait.impl/std/os/fd/raw/trait.AsRawFd.js
+++ trait.impl/std/os/fd/raw/trait.AsRawFd.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rustix",[]],["wayland_server",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/std/os/fd/raw/trait.AsRawFd.html\" title=\"trait std::os::fd::raw::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"wayland_server/struct.ListeningSocket.html\" title=\"struct wayland_server::ListeningSocket\">ListeningSocket</a>"]]]]);
+    var implementors = Object.fromEntries([["rustix",[]],["wayland_server",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/std/os/fd/raw/trait.AsRawFd.html\" title=\"trait std::os::fd::raw::AsRawFd\">AsRawFd</a> for <a class=\"struct\" href=\"wayland_server/struct.ListeningSocket.html\" title=\"struct wayland_server::ListeningSocket\">ListeningSocket</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {

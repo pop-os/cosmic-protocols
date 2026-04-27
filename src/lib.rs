c32a1f59f556456e11e3870b61114943a34b6272
Michael Aaron Murphy <michael@mmurphy.dev>
2026-04-27 14:51:38 +0200

    docs: doc_auto_cfg feature removed in Rust 1.92
    This is now included in the doc_cfg feature.

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@
 
 #![warn(missing_docs)]
 #![forbid(improper_ctypes, unsafe_op_in_unsafe_fn)]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(rustfmt, rustfmt_skip)]
 
 
